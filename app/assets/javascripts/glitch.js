a4af1bc0c7cc05a12ea040fcc9b41310d094b15c
Walid <walidvb@gmail.com>
2015-02-25 22:36:13 +0000

    adds grayscale to glitch effect

--- app/assets/javascripts/glitch.js
+++ app/assets/javascripts/glitch.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
   // assuming there's a loaded img and a canvas element in the DOM.
   var myGlitches = document.getElementsByTagName('img');
-  console.log("myGlitches.length:", myGlitches.length);
   for(var i = 0; i < myGlitches.length; i++){
     var currentGlitch = myGlitches[i];
-    console.log("i, currentGlitch:", i, currentGlitch);
     if(currentGlitch.nodeName == "IMG"){
       currentGlitch.onload = function(){
         var originalImg = new Image();
@@ -35,28 +33,44 @@ $(document).ready(function(){
   }
 
   function glitchOnHover(originalImg, canvas, ctx){
-        var my_img_data = ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
-        var glitchInterval = null;
-        function drawOriginalImage() {
-          clearInterval(glitchInterval);
-          ctx.putImageData(my_img_data, 0, 0);
-        }
+    var my_img_data = ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
+    var glitchInterval = null;
+    function drawOriginalImage() {
+      clearInterval(glitchInterval);
+      ctx.putImageData(my_img_data, 0, 0);
+    }
 
-        function drawGlitchedImageData(img_data) {
-          ctx.putImageData(img_data, 0, 0);
-        }
-        canvas.onmouseout = drawOriginalImage;
-        canvas.onmouseover = function(){
-          glitchInterval = setInterval(glitchIt, 200);
-        };
-        function glitchIt(){
-          var parameters = {
-            amount: Math.random()*100,
-            seed: Math.random()*100,
-            iterations: randomInt(0, 70),
-            quality: Math.random()*100
-          };
-          glitch(my_img_data, parameters, drawGlitchedImageData);
-        }
-      }
+    function drawGlitchedImageData(img_data) {
+      var rdm = Math.random() > 0.4;
+      grayscaleImg = rdm ? img_data : grayscale(img_data);
+      ctx.putImageData(grayscaleImg, 0, 0);
+    }
+    canvas.onmouseout = drawOriginalImage;
+    canvas.onmouseover = function(){
+      glitchInterval = setInterval(glitchIt, 200);
+    };
+    function glitchIt(){
+      var parameters = {
+        amount: Math.random()*100,
+        seed: Math.random()*100,
+        iterations: randomInt(0, 70),
+        quality: Math.random()*100
+      };
+      glitch(my_img_data, parameters, drawGlitchedImageData);
+    }
+    function grayscale(pixels, args) {
+      var d = pixels.data;
+      for (var i=0; i<d.length; i+=4) 
+      {
+        var r = d[i];
+        var g = d[i+1];
+        var b = d[i+2];
+      // CIE luminance for the RGB
+      // The human eye is bad at seeing red and blue, so we de-emphasize them.
+      var v = 0.2126*r + 0.7152*g + 0.0722*b;
+      d[i] = d[i+1] = d[i+2] = v
+    }
+    return pixels;
+  }
+}
 });
