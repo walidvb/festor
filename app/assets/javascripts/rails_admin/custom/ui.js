9a09f94309da7dd4453da4bd222b35472fd2fdaf
Walid <walidvb@gmail.com>
2015-02-28 15:22:51 +0000

    adds image button

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,13 +1,14 @@
-//require froala/js/froala_editor.min.js
-//require froala/js/plugins/file_upload.min.js
-//require froala/js/plugins/videos.min.js
-//require froala/js/plugins/urls.min.js
+//= require froala/js/froala_editor.min.js
+//= require froala/js/plugins/file_upload.min.js
+//= require froala/js/plugins/video.min.js
+//= require froala/js/plugins/urls.min.js
+
 $(document).on('rails_admin.dom_ready', function(){
 	$('textarea').editable({
 		inlineMode: false,
 		plainPaste: true,
 		paragraphy: false,
 		placeholder: "Edit Me!",
-		buttons: ['undo', 'redo' , 'sep', 'bold', 'italic', 'underline', 'sep', 'link', 'sep', 'formatBlock', 'sep', 'html']
+		buttons: ['undo', 'redo' , 'sep', 'bold', 'italic', 'underline', 'sep', 'link', 'sep', 'formatBlock','sep', 'insertImage', 'insertVideo', 'uploadFile', 'sep', 'html']
 	})
 });
