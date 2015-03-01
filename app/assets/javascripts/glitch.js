1c1e15687937c54442415f767e0dc2e25590e0ec
Walid <walidvb@gmail.com>
2015-03-01 22:33:18 +0000

    refactors glitch.js

--- app/assets/javascripts/glitch.js
+++ app/assets/javascripts/glitch.js
@@ -1,24 +1,16 @@
 $(document).ready(function(){
   // assuming there's a loaded img and a canvas element in the DOM.
   var myGlitches = document.getElementsByClassName('glitch');
+  var glitchables = [];
+  window.glitchables = glitchables;
   for(var i = 0; i < myGlitches.length; i++){
     var currentGlitch = myGlitches[i];
     if(currentGlitch.nodeName == "IMG"){
       currentGlitch.onload = function(){
-        var originalImg = new Image();
-        originalImg.src = this.src;
-        originalImg.crossOrigin = "*"
-        console.log("originalImg.src:", originalImg.src);
-        var canvas = document.createElement('canvas');
-        var width = originalImg.width;
-        var height = originalImg.height;
-        canvas.width = width;
-        canvas.height = height;
-        $(this).replaceWith(canvas);
-        var ctx = canvas.getContext('2d');
-        // draw the img on the canvas 
-        ctx.drawImage(originalImg, 0, 0);
-        glitchOnHover(originalImg, canvas, ctx);
+        var foo = new Glitchable(this);
+        foo.init();
+        glitchables.push(foo);
+        foo.glitchOnHover();
       }
     }
     else if(currentGlitch.nodeName == 'A')
@@ -28,50 +20,82 @@ $(document).ready(function(){
       rasterizeHTML.drawHTML( html );
     }
   }
+  function Glitchable(img){
+    this.originalImg = img;
+  }
 
+  Glitchable.prototype.init = function(){
+    var originalImgClone = new Image();
+    originalImgClone.src = this.originalImg.src;
+    originalImgClone.crossOrigin = "*"
+    console.log("originalImg.src:", this.originalImg.src);
+    var canvas = document.createElement('canvas');
+    var width = originalImgClone.width;
+    var height = originalImgClone.height;
+    canvas.width = width;
+    canvas.height = height;
+    $(this.originalImg).replaceWith(canvas);
+    ctx = canvas.getContext('2d');
+    // draw the img on the canvas 
+    ctx.drawImage(originalImgClone, 0, 0);
+    this.originalImgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.glitchOnHover();
+  };
+
+  Glitchable.prototype.glitchOnHover = function(){
+    var glitchInterval = null;
+    var that = this;
+    this.canvas.onmouseover = function(){
+      console.log("mouovering:", that);
+      glitchInterval = setInterval(that.glitchIt.bind(that), 200);
+    };
+    this.canvas.onmouseout = function() {
+      clearInterval(glitchInterval);
+      that.ctx.putImageData(that.originalImgData, 0, 0);
+    }
+  };
+  
+  Glitchable.prototype.glitchIt = function(parameters){
+    var parameters = parameters || {
+      amount: Math.random()*100,
+      seed: Math.random()*100,
+      iterations: randomInt(0, 70),
+      quality: Math.random()*100
+    };
+    var that = this;
+    glitch(this.originalImgData, parameters, function(img_data) {
+      var rdm = Math.random() > 0.4;
+      grayscaleImg = rdm ? img_data : grayscale(img_data);
+      that.ctx.putImageData(grayscaleImg, 0, 0);
+    });
+  };
+
+  
+  function getRandomVisibleCanvas(glitchables){
+    for(var i = 0; i < glitchables.length; i++){
+      var $canvas = $(glitchables[i].canvas);
+      var top = $canvas.offset().top;
+      return top + $canvas.height() < window.scrollTop() ||top > window.scrollTop() + window.innerHeight
+    }
+  }
+  // Utils
   function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
-  function glitchOnHover(originalImg, canvas, ctx){
-    var my_img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var glitchInterval = null;
-    function drawOriginalImage() {
-      clearInterval(glitchInterval);
-      ctx.putImageData(my_img_data, 0, 0);
-    }
-
-    function drawGlitchedImageData(img_data) {
-      var rdm = Math.random() > 0.4;
-      grayscaleImg = rdm ? img_data : grayscale(img_data);
-      ctx.putImageData(grayscaleImg, 0, 0);
-    }
-    canvas.onmouseout = drawOriginalImage;
-    canvas.onmouseover = function(){
-      glitchInterval = setInterval(glitchIt, 200);
-    };
-    function glitchIt(){
-      var parameters = {
-        amount: Math.random()*100,
-        seed: Math.random()*100,
-        iterations: randomInt(0, 70),
-        quality: Math.random()*100
-      };
-      glitch(my_img_data, parameters, drawGlitchedImageData);
-    }
-    function grayscale(pixels, args) {
-      var d = pixels.data;
-      for (var i=0; i<d.length; i+=4) 
-      {
-        var r = d[i];
-        var g = d[i+1];
-        var b = d[i+2];
-      // CIE luminance for the RGB
-      // The human eye is bad at seeing red and blue, so we de-emphasize them.
-      var v = 0.2126*r + 0.7152*g + 0.0722*b;
-      d[i] = d[i+1] = d[i+2] = v
+  function grayscale(pixels, args) {
+    var d = pixels.data;
+    for (var i=0; i<d.length; i+=4) 
+    {
+      var r = d[i];
+      var g = d[i+1];
+      var b = d[i+2];
+    // CIE luminance for the RGB
+    // The human eye is bad at seeing red and blue, so we de-emphasize them.
+    var v = 0.2126*r + 0.7152*g + 0.0722*b;
+    d[i] = d[i+1] = d[i+2] = v
     }
     return pixels;
   }
-}
 });
