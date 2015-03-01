1daa5165430792ebb449c28ab642125992437d1b
Walid <walidvb@gmail.com>
2015-03-01 20:12:36 +0000

    adds and styles filters

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,11 +1,20 @@
 
 $(document).ready(function(){
+
 	$('#single-events-list').mixItUp({
 		selectors: {
 			target: '.event'
 		},
+		load: {
+      filter: '.perfos,.clubbing'
+    },
+    controls: {
+      toggleFilterButtons: true
+    },
+    callbacks: {
+		},
 		animation: {
-			effects: 'stagger fade scale(0.9)',
+			effects: 'stagger fade scale(0.8)',
 			staggerSequence: function(i){
 				return (2*i) - (5*((i/3) - ((1/3) * (i%3))));
 			}
