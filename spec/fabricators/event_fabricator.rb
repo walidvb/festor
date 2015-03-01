a888c11747280c453b4b7d9fc5a6b6d0f6fbdd1d
Walid <walidvb@gmail.com>
2015-03-01 00:02:46 +0000

    adds tests

--- spec/fabricators/event_fabricator.rb
+++ spec/fabricators/event_fabricator.rb
@@ -1,6 +1,10 @@
 Fabricator(:event) do 
-	title {|attrs| "The #{attrs[:type]}"}
-	description	"The description"
+	title						{|attrs| "The #{attrs[:type]}"}
+	description			"The description"
 	schedule_start	3.days.from_now
 	buy_link				"http://some-long-link/a-path?and=params"
+	main_image_file_name 				{ 'image.png' }
+  main_image_content_type 		{ 'image/jpeg' }
+  main_image_file_size 				{ 1024 }
+  main_image									nil
 end
