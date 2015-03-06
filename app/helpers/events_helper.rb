42c45e6b15c4d5a4e7f1c67d2e8ddd202684ba42
Walid <walidvb@gmail.com>
2015-03-06 12:54:44 +0000

    adds link to info page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,7 +3,7 @@ module EventsHelper
 		start = event.schedule_start
 		finish = event.schedule_end
 		if finish.present?
-			"#{start.strftime('%e')} t(:au) #{finish.strftime('%e')} &mdash; #{start.strftime('%H:%M')}-#{finish.strftime('%H:%M')}".html_safe
+			"#{start.strftime('%e')} #{t(:au)} #{finish.strftime('%e')} &mdash; #{start.strftime('%H:%M')}-#{finish.strftime('%H:%M')}".html_safe
 		else
 			"#{start.strftime('%e')} &mdash; #{start.strftime('%H:%M')}".html_safe
 		end
--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,11 +1,19 @@
 class StaticPage < ActiveRecord::Base
 	translates :body, :title, :slug
 	accepts_nested_attributes_for :translations, allow_destroy: true
+	before_destroy :prevent_destroy_static_pages
 
 	validates :body, presence: true
 	scope :static, ->{where(news: false).order(:position)}
 	scope :public, ->{where(public: true)}
 	scope :news, ->{where(news: true).order(:created_at)}
+
+	def prevent_destroy_static_pages
+		if !self.news
+			self.errors[:base] << "You cannot destroy static pages."
+			return false 
+		end
+	end
 
 	### rails_admin
 	rails_admin do
