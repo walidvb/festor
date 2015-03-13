17af1a08ad4589c82e795e12b990ad8dc326ee08
Walid <walidvb@gmail.com>
2015-03-13 10:26:27 +0000

    bumps up asset version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,9 @@ Festor::Application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
   config.assets.prefix = "static"
-  
+
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.2'
+  config.assets.version = '1.2.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
