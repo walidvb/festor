a318c16a9b0e7c8a4f331889afa7763917a1f4cf
Walid <walidvb@gmail.com>
2015-03-13 12:28:36 +0000

    fixes prev/next

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
 	before_filter :get_type, only: [:index]
 
 	def index
-		@events = Event.includes(:artists, :location).send(@type.to_sym)
+		@events = Event.includes(:artists, :location).send(@type.to_sym).order(:schedule_start, :schedule_end)
 		if @type == :single_event
 			@dates = @events.map(&:schedule_start).uniq{|d| d.strftime("%e-%b-%y")}.compact.sort()
 			render 'index_single_events'
@@ -13,6 +13,10 @@ class EventsController < ApplicationController
 
 	def show
 		@event = Event.includes(:artists, :location).friendly.find(params[:id])
+		events_array = Event.select(:id).order(:schedule_start, :schedule_end).map(&:id)
+		current_index = events_array.index(@event.id)
+		@next = events_array[current_index + 1]
+		@previous = events_array[[0, current_index - 1].max]
 		@type = @event.type
 		@musicians = @event.musicians
 		@vjs = @event.vjs
