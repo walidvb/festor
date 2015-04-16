5a521f01baf8f742a580d06627a63931d45be5d0
Walid <walidvb@gmail.com>
2015-04-16 11:45:30 +0200

    sorts artists by date

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,12 @@
 class ArtistsController < ApplicationController
 	
 	def index
-		@artists = Artist.all
+		@artists = Artist.includes(:events).sort{|a,b| a.events.first.schedule_start <=> b.events.first.schedule_start}
 	end
 
 	def show
-		@artist = Artist.includes(:bookings).friendly.find(params[:id])
-		@bookings = @artist.bookings
+
+		@artist = Artist.includes(:events).friendly.find(params[:id])
 		@links = @artist.links
 	end
 end
