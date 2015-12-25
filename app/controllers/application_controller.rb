c303c609beec688ce9240426b39511c0e655c856
Walid <walidvb@gmail.com>
2015-12-25 18:46:09 +0100

    adds beta filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,32 +4,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+  before_filter :beta_only
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  
+
 
   # Devise permitted params
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-      :email, 
-      :password, 
-      :password_confirmation) 
+      :email,
+      :password,
+      :password_confirmation)
     }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
-      :email, 
-      :password, 
+      :email,
+      :password,
       :password_confirmation,
       :current_password
-      ) 
+      )
     }
   end
-  
+
   # Redirects on successful sign in
   def after_sign_in_path_for(resource)
     cookies.permanent[:beta] = "sign"
     rails_admin_path
   end
-  
+
   # Only permits admin users
   def require_admin!
     authenticate_user!
