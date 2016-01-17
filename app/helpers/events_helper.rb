4024b29d9d160bbbac268cd22172d21b87334ba7
Walid <walidvb@gmail.com>
2016-01-17 18:11:16 +0100

    cleanup

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,7 +4,6 @@ module EventsHelper
 		result = event.description
 			.gsub('[image]', image_tag(event.main_image.url(:large), class: 'inline-image'))
 			.gsub('[info]', infos)
-			# this should also render the infos from the partial
 		auto_html(result) {
       youtube(:width => 600, :height => 375)
       vimeo(:width => 600, :height => 375)
@@ -15,6 +14,5 @@ module EventsHelper
       link(:target => 'blank')
       image
 		}
-		#mf_auto_html(result)
 	end
 end
