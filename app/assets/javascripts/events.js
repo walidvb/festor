c9d04b829527220e287d1ca609ef2a8d5ce0f6ef
Walid <walidvb@gmail.com>
2016-01-17 23:24:11 +0100

    finishes artist carousel

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,14 +1,21 @@
-$(document, '[data-load-more]').on('click', function(e){
+$(document, '.close').click(function(e){
+  $('.load-more-container').hide();
+  $('#main-content').show();
+  $('.close').hide();
+});
 
+$(document, '[data-load-more]').on('click', function(e){
   var $this = $(e.target).parent('[data-load-more]');
-  if($this){
+  console.log($this);
+  if($this.length){
+    console.log('getting ', $this.data('load-more'));
     e.preventDefault();
     $.ajax({
       url: $this.data('load-more'),
       success: function(data){
-        debugger;
+        $('.close').show();
         $('#main-content').hide();
-        $('.load-more-container').append($(data).find('#main-content'));
+        $('.load-more-container').html($(data).find('#main-content')).show();
       }
     })
   };
