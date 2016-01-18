5a2afa3f6eba5bd22c668f6113b9046c04e7f52d
Walid <walidvb@gmail.com>
2016-01-18 02:29:36 +0100

    hopefully last play with the logo

--- app/assets/javascripts/logo.js
+++ app/assets/javascripts/logo.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
   var target = $('#banner');
-  $('#banner').mousemove(function(e){
+  target.mousemove(function(e){
     var $this = $(this);
+    var yInDiv = event.clientY - this.offsetTop;
     var totalHeight = $this.height()/2;
-    var yRatio = (event.clientY - totalHeight) / totalHeight;
-    var yDiffToMiddle = event.clientY - totalHeight;
+    var yRatio = (yInDiv - totalHeight) / totalHeight;
+    var yDiffToMiddle = yInDiv - totalHeight;
     var dist = map(yDiffToMiddle, -0.5, 0.5, -totalHeight, totalHeight);
 
+    var xInDiv = event.clientX - this.offsetLeft;
     var totalWidth = $this.width()/2;
-    var xRatio = (event.clientX - totalWidth) / totalWidth;
-    var xDiffToMiddle = event.clientX - totalWidth;
-    dist = map(xDiffToMiddle, -0.3, 0.3, 0, totalWidth);
-
+    var xDiffToMiddle = xInDiv - totalWidth;
+    dist = map(-xDiffToMiddle, -0.5, 0.5, -totalWidth, totalWidth);
+    $this.toggleClass('shadow-opposite', (dist < 0));
+    console.log(xInDiv, '. ', xDiffToMiddle, 'dist: ', dist);
     target.css({
       'text-shadow': dist + 'em 0 0px black, '+dist*2+'em 0 0px black'
     });
-    console.log(dist + 'em 0 0px black, '+dist*2+'em 0 0px black');
   }).mouseleave(function(){
     $(this).attr('style', '');
   });
