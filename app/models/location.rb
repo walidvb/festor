1582eeffcc6fa61fc07857c4ab8ffd89b3b35ef8
Walid <walidvb@gmail.com>
2016-01-26 14:20:22 -0200

    removes images from locations in model

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,15 +9,6 @@ class Location < ActiveRecord::Base
 	has_many :bookings, through: :events, inverse_of: :location
 	has_many :artists, through: :bookings, inverse_of: :location
 
-	has_attached_file :picture,
-	:styles => {
-		:thumb => "100x100#",
-		:small => "400x300#",
-		:large => "800x600#"
-	},
-	:default_url => "/images/missing.jpg",
-	:use_timestamp => false
-	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
 	private
 	def address_with_country
