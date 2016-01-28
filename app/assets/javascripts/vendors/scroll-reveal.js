e24b459e95c4786ba001d38182bbc75de986b8a4
Walid <walidvb@gmail.com>
2016-01-28 17:50:26 -0200

    adds logging to debug 3

--- app/assets/javascripts/vendors/scroll-reveal.js
+++ app/assets/javascripts/vendors/scroll-reveal.js
@@ -1,5 +1,6 @@
 (function($, window){
   window.debounceTimeout = 10;
+  window.leading = true;
   $.fn.scrollReveal = function(options_){
     var that = this;
     var offsetTop, getTop;
@@ -54,7 +55,7 @@
         Math.abs(distCenter) <= $this.height()/2 ? addActive() :
         addTopOrBottom(distCenter > 0);
       });
-    }, window.debounceTimeout);
+    }, window.debounceTimeout, window.leading);
 
     if(window.hasOwnProperty('ontouchstart'))
     {
@@ -73,23 +74,24 @@
     }
 
   }
-  // Returns a function, that, as long as it continues to be invoked, will not
-  // be triggered. The function will be called after it stops being called for
-  // N milliseconds. If `immediate` is passed, trigger the function on the
-  // leading edge, instead of the trailing.
-  function debounce(func, wait, immediate) {
-  	var timeout;
-  	return function() {
-      console.log('timeout', window.debounceTimeout);
-  		var context = this, args = arguments;
-  		var later = function() {
-  			timeout = null;
-  			if (!immediate) func.apply(context, args);
-  		};
-  		var callNow = immediate && !timeout;
-  		clearTimeout(timeout);
-  		timeout = setTimeout(later, wait);
-  		if (callNow) func.apply(context, args);
-  	};
-  };
 })(jQuery, window);
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    console.log('wait', wait);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
