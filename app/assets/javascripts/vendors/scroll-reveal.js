4fda26a0db11f3aad246d022301721f9564df681
Walid <walidvb@gmail.com>
2016-01-28 17:24:09 -0200

    debounces scroll-reveal

--- app/assets/javascripts/vendors/scroll-reveal.js
+++ app/assets/javascripts/vendors/scroll-reveal.js
@@ -1,13 +1,12 @@
 (function($, window){
   $.fn.scrollReveal = function(options_){
     var that = this;
-    console.log(that);
     var offsetTop, getTop;
+
     var options = $.extend({
       maxDistanceToCenter: window.innerHeight*0.15,
       itemSelector: '.grid-item',
       activeClass: 'sr-active',
-      shouldShow: shouldShow,
       touchOnly: true,
       mCustomScrollbar: {
         autoHideScrollbar: true,
@@ -28,23 +27,10 @@
 
     var elems = $(options.itemSelector);
     if(!elems.length) return;
-    if(window.hasOwnProperty('ontouchstart'))
-    {
-      $(that).css('overflow', 'hidden');
-      $(that).mCustomScrollbar(options.mCustomScrollbar);
 
-    }
-    else{
-      getTop = function(){
-        return $(window).scrollTop();
-      };
-      offsetTop = function(elem){
-        return elem.offset().top;
-      }
-      $(window).on('scroll', onScroll);
-    }
 
-    function onScroll(){
+    var onScroll = debounce(function(){
+      console.log('running');
       elems.each(function(){
         var $this = $(this);
 
@@ -65,12 +51,46 @@
           }
         }
         Math.abs(distCenter) <= $this.height()/2 ? addActive() :
-          addTopOrBottom(distCenter > 0);
+        addTopOrBottom(distCenter > 0);
 
       });
+    }, 15);
+
+
+    if(window.hasOwnProperty('ontouchstart'))
+    {
+      $(that).css('overflow', 'hidden');
+      $(that).mCustomScrollbar(options.mCustomScrollbar);
+
     }
-    function shouldShow(elem){
+    else{
+      getTop = function(){
+        return $(window).scrollTop();
+      };
+      offsetTop = function(elem){
+        return elem.offset().top;
+      }
+      $(window).on('scroll', onScroll);
     }
 
   }
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  function debounce(func, wait, immediate) {
+  	var timeout;
+  	return function() {
+  		var context = this, args = arguments;
+  		var later = function() {
+  			timeout = null;
+  			if (!immediate) func.apply(context, args);
+  		};
+  		var callNow = immediate && !timeout;
+  		clearTimeout(timeout);
+  		timeout = setTimeout(later, wait);
+      debugger;
+  		if (callNow) func.apply(context, args);
+  	};
+  };
 })(jQuery, window);
