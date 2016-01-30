093e9579a44cd60d1b0130d481bf137373c4319a
Walid <walidvb@gmail.com>
2016-01-29 23:50:46 -0200

    styles nav for mobile

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,26 +1,11 @@
 
 $(document).ready(function() {
-	var options = {
-		maxWidth: "95%",
-		maxHeight: "95%",
-	};
-
-	$('.colorbox.trigger').colorbox(options);
-	$('.colorbox.image').colorbox(options);
-	$('.colorbox.video').each(function(){
-		var href = $(this).attr('href');
-		if(/youtube/.test(href)){
-			options.width = 1120;
-			options.height = 630;
-		}
-		else
-		{
-			options.width = 1000;
-			options.height = 562;
-		}
-		options.iframe= true;
-		$(this).colorbox(options);
-	});
+	if(window.innerWidth < 767){
+		var header = $('header');
+		$('.header-left, .header-left a').click(function(e){
+			header.toggleClass('open');
+		});
+	}
 
 	$('.grid').imagesLoaded( function() {
 		$('body').scrollReveal();
