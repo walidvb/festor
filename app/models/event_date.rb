ec79b2f4a3680684b9623236df369bdfe3fb6a42
Walid <walidvb@gmail.com>
2016-02-01 17:32:28 -0200

    removes polymorphic as not polymorphic

--- app/models/event_date.rb
+++ app/models/event_date.rb
@@ -1,5 +1,5 @@
 class EventDate < ActiveRecord::Base
-  belongs_to :dateable, inverse_of: :event_dates#, polymorphic: true
+  belongs_to :dateable, inverse_of: :event_dates, class_name: 'Event'#, polymorphic: true
   validates_presence_of :dateable, :start
 
   def title
