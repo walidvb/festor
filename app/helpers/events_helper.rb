aa0a743b2b003455bc60f23991cd97f2f885ea7f
Walid <walidvb@gmail.com>
2016-02-05 11:38:35 -0200

    closes #33

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,7 @@
 module EventsHelper
   def path_for event, options = {locale: I18n.locale}
-    cat = event.category
+
+    cat = event.category.to_sym
     Event.workshop_cats.include?(cat) ? workshop_path(event, locale: options[:locale]) :
       cat == :exhibition ? exhibition_path(event, locale: options[:locale]) :
         event_path(event, locale: options[:locale])
