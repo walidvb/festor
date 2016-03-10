fba1ddb0742cb4f6cd7c4e4517869636c9a2469c
Walid <walidvb@gmail.com>
2016-03-10 11:48:31 +0100

    adds hovering on dates

--- app/assets/javascripts/program.js
+++ app/assets/javascripts/program.js
@@ -1,24 +1,44 @@
 $(document).on('turbolinks:load', function(){
   $('#program').on('arrangeComplete', initProgram);
-})
-$(window).on('resize', initProgram);
-function initProgram(){
-  var dayElems = $('.day');
-  var days = _.map(dayElems, function(elem){
-    return $(elem).data('date');
-  });
-  var minOffset = 0;
-  for(var i = 0; i < dayElems.length; i++){
-    var day = days[i];
-    var $this = $(dayElems[i]);
-    var offset = parseInt($('.grid .'+day+':visible').first().css('top'));
-    if(typeof(offset) != "number"){
-      $this.fadeOut();
+  $(window).on('resize', initProgram);
+  function initProgram(){
+    var dayElems = $('.day');
+    var days = _.map(dayElems, function(elem){
+      return $(elem).data('date');
+    });
+    var minOffset = 0;
+    for(var i = 0; i < dayElems.length; i++){
+      placeDate(i);
+      bindHoverDate(i);
+    };
+
+    function placeDate(i){
+      var day = days[i];
+      var $this = $(dayElems[i]);
+      var firstVisibleEvent = $this.nextAll('.grid-item.'+day+':visible').first();
+      if(!firstVisibleEvent.length){
+        $this.fadeOut();
+      }
+      else{
+        var offset = parseInt(firstVisibleEvent.css('top'));
+        $this.fadeIn();
+        $this.css({top: offset >= minOffset ? offset : minOffset})
+        minOffset = offset + $this.height();
+      }
+    };
+
+    function bindHoverDate(i){
+      var elem = $(dayElems[i]);
+      elem.hover(function(){focusDate(i)}, unfocusDate);
+      function unfocusDate(i){
+        $('.focused').removeClass('focused');
+      }
+    };
+    function focusDate(i){
+      console.log(days[i]);
+      $('.grid-item:not(.'+days[i]+')').removeClass('focused');
+      $('.grid-item.'+days[i]).addClass('focused');
     }
-    else{
-      $this.fadeIn();
-    }
-    $this.animate({top: offset >= minOffset ? offset : minOffset})
-    minOffset = offset + $this.height();
+
   }
-}
+})
