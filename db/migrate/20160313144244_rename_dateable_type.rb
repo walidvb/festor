ba448472411b6f3be8e058696c298f34ae2c7415
Walid <walidvb@gmail.com>
2016-03-13 15:57:32 +0100

    fix migration

--- db/migrate/20160313144244_rename_dateable_type.rb
+++ db/migrate/20160313144244_rename_dateable_type.rb
@@ -1,5 +1,5 @@
 class RenameDateableType < ActiveRecord::Migration
   def change
-    rename_column :event_dates, :event_id, :event_id
+    rename_column :event_dates, :dateable_id, :event_id
   end
 end
