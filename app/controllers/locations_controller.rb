435eac986d3c1260c0a474e831b94f37843641c6
Walid <walidvb@gmail.com>
2016-04-04 02:23:03 +0200

    make all locations public

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,13 +2,6 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.order('name ASC').includes(:events).all
     @events = @locations.map(&:events).flatten
-    if !user_signed_in?
-      @locations.select! do |loc|
-        loc.events.map(&:is_workshop?).include?(true)
-      end
-      @events.select! do |ev|
-        ev.is_workshop?
-      end
     end
   end
 end
