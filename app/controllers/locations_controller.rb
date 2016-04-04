daa18f237fe8d20768bf78dd29e857d0a7dfe222
Walid <walidvb@gmail.com>
2016-04-04 02:26:07 +0200

    typo

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,5 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.order('name ASC').includes(:events).all
     @events = @locations.map(&:events).flatten
-    end
   end
 end
