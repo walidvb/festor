93c0d483294cff3e5e462c10d5891481b1b209ad
Walid <walidvb@gmail.com>
2016-04-05 20:06:49 +0200

    more logs

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -59,6 +59,7 @@ class ScreenshotsController < ApplicationController
       loc = request.location
       puts loc.inspect
       puts loc.country
+      puts loc.country_code
       params.require(:screenshot).permit(:ip, :screenshot, :user_agent, :name).merge({
         ip: request.ip,
         city: loc.city,
