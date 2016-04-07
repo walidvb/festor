74c0b9e51e5898651977b7bcc1981e97d6af19f3
Walid <walidvb@gmail.com>
2016-04-07 11:55:48 +0200

    rm pause on hover

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,7 +11,6 @@ $(document).on('turbolinks:load', function(){
 
 	if($.fn.marquee){
 		$('.news').marquee({
-			pauseOnHover: true,
 			startVisible: false,
 			pauseOnCycle: true,
 			duration: 8000,
