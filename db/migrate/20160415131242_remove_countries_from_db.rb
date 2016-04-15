f017401f89281cbc6b75fdbe39eba03eb4e56030
Walid <walidvb@gmail.com>
2016-04-15 15:29:52 +0200

    switch back to google

---
+++ db/migrate/20160415131242_remove_countries_from_db.rb
@@ -0,0 +1,7 @@
+class RemoveCountriesFromDb < ActiveRecord::Migration
+  def change
+    drop_table :maxmind_geolite_city_blocks
+    drop_table :maxmind_geolite_city_location
+    drop_table :maxmind_geolite_country
+  end
+end
