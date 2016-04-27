37213214f0e3259933fd4316db05da93081e55df
Walid <walidvb@gmail.com>
2016-04-27 22:45:28 +0200

    fix for nightly

--- app/assets/javascripts/lines.js
+++ app/assets/javascripts/lines.js
@@ -26,7 +26,7 @@ function Nighter(){
 
   this.init = function(){
     var now = new Date();
-    that.isNight = (now.getHours() > 20 && now.getHours > 38);
+    that.isNight = (now.getHours() > 20 && now.getMinutes > 38) || now.getHours() > 21;
     that.apply();
     $('head').append(styleTag);
   }
