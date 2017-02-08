03c527e1a901c0190c3e2eb4bd7258c28e40f447
Walid <walidvb@gmail.com>
2017-02-08 16:19:35 +0100

    restores archives

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -4,6 +4,7 @@ function hideHeader(e){
 	$('header').removeClass('open');
 }
 $(document).on('turbolinks:load', function() {
+	return;
 	$('.header-left').on('click', function(e){
 		console.log('toggling menu');
 		var header = $('header');
