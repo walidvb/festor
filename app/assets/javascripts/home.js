031ad4471856c87c3e448f35565e551abaa32321
Walid <walidvb@gmail.com>
2017-02-14 22:43:38 +0100

    zoom logo

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -16,4 +16,55 @@ $(document).on('turbolinks:load', function(){
   })
 });
 $(document).on('turbolinks:load', function(){
+	if(!smallScreen()){
+		const $logo = $('header'),
+			$nav = $('nav');
+		const $logoHeight = $logo.outerHeight(true);
+
+		const handleScrollHeader = ({ currentTarget }) => {
+			const scrollTop = $(currentTarget).scrollTop();
+			const MIN = 10,
+				MAX = 300;
+			let opacity,
+				translateZ,
+				top;
+			if(scrollTop < MIN){
+				opacity = 1;
+				translateZ = 0;
+				top = $logoHeight;
+			}
+			else if(scrollTop > MIN && scrollTop < MAX){
+				opacity = map(scrollTop, MIN, MAX, 1, 0.3);
+				translateZ = map(scrollTop, MIN, MAX, 0, 10);
+				 top = map(scrollTop, MIN, MAX, $logoHeight, $logoHeight*3/4)
+			}
+			else{
+				opacity = .3;
+				translateZ = 10;
+				top = $logoHeight*3/4;
+			}
+			console.log('asd');
+			setPropertyWithoutTransition($logo, {
+				transform: `perspective(60px) translateZ(${-translateZ}px)`,
+				opacity,
+			});
+
+			setPropertyWithoutTransition($nav, {
+				height: `calc(100vh - ${top}px)`
+			});
+
+			function setPropertyWithoutTransition(elem, props){
+				props.transition = 'all 0s';
+				elem.css(props);
+				elem.css({
+					transition: "",
+				})
+			}
+		}
+		$('main').on('scroll', handleScrollHeader);//debounce(, 20, true));
+	}
 })
+
+function map(num, inMin, inMax, outMin, outMax){
+	return (num - inMin)*(outMax - outMin) / (inMax - inMin) + outMin;
+}
