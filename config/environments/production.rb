565efbd2743020296e5b99a47506949990fa5998
Walid <walidvb@gmail.com>
2017-02-15 19:31:33 +0100

    adds gzip for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.middleware.use Rack::Deflater
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
