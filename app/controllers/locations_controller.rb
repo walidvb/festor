e844c3d2af1d7277efae00aa9e2bc83505dddd51
Walid <walidvb@gmail.com>
2017-03-23 17:10:37 +0100

    fix location navigation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,5 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.order('name ASC').includes(:events).all
     @events = @locations.map(&:events).flatten
   end
 end
